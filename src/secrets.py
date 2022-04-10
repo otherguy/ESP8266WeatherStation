bb5b5ee23d0e2a998fb7bcc2127cff8dc3216c46
Alexander Graf <alex@basecamp.tirol>
2022-04-10 15:12:37 +0200

    Add secrets.py

---
+++ src/secrets.py
@@ -0,0 +1,18 @@
+# Do not commit changes to this file to VCS
+# git update-index --assume-unchanged src/secrets.py
+
+# WiFi credentials
+WIFI_SSID = "<YOUR WIFI SSID>"
+WIFI_PASSWORD = "<YOUR WIFI PASSWORD>"
+
+# Weather Conditions
+GEO_NAME = 'New York City, US'
+GEO_LAT = 40.7143
+GEO_LON = -74.006
+
+# https://home.openweathermap.org/api_keys
+OW_API_KEY = "<YOUR API KEY>"
+
+# Display Off
+DISPLAY_OFF_START = 23 # 23:00
+DISPLAY_OFF_END   = 7  # 7:00
